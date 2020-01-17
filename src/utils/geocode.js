83baaff9f2b68ac7017b1302cb6cfc19b067d20d
iveloso <ian.veloso@telus.com>
2020-01-17 11:05:05 -0800

    Support for proxy in dev environment. Added nodemon as devDependency

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,9 +1,10 @@
 const request = require('request')
+const proxy = process.env.PROXY || ''
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiaWFudmVsb3NvIiwiYSI6ImNrNWNwbHFlYTAydG0zZnNkOXgyemp0Y2wifQ.Tw4YbPNeKpzS0otPrK-dBA&limit=1'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true, proxy: proxy }, (error, response) => {
         if(error) {
             callback('Unable to connect to location services!')
         } else if(response.body.features.length == 0) {
--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,9 +1,10 @@
 const request = require('request')
+const proxy = process.env.PROXY || ''
 
 const weather = ({latitude, longitude, location}, callback) => {
     const url = 'https://api.darksky.net/forecast/04f72884c3ddcb10a5a8c8de4fb11330/' + latitude + ',' + longitude + '?units=si&lang=en'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true, proxy: proxy }, (error, response) => {
         if(error) {
             callback('Unable to connect to weather service', null)
         } else if(response.body.error) {
