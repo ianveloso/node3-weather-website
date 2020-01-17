4673d4cc84f623449be5f967f2de27951cef73a4
ianveloso <54689562+ianveloso@users.noreply.github.com>
2020-01-16 17:18:14 -0800

    test commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded')
-
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
 const message1Element = document.querySelector('#message-1')
